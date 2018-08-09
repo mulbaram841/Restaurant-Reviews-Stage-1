df24aeb3e841e4ccd1796335a63959fd6ae1b821
Mara Lisauskas <mlisau@gmail.com>
2018-08-09 14:43:07 -0700

    Update restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<pk.eyJ1IjoibXVsYmFyYW04NDEiLCJhIjoiY2prZzgzYTNoMDR1MDNwbGh4YTFocmh3bSJ9.FrX4ngELrJ6OKKTLyyyNzQ>',
+        mapboxToken: 'pk.eyJ1IjoibXVsYmFyYW04NDEiLCJhIjoiY2prZzgzYTNoMDR1MDNwbGh4YTFocmh3bSJ9.FrX4ngELrJ6OKKTLyyyNzQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
